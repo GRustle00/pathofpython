984b1c5a3c46fcb7e4111ae5bc5ba10e9356951e
Guillermo Fernandez <gfernandez@localhost.localdomain>
2020-07-30 08:54:25 -0400

    adding new exercises to lpthw folder

---
+++ lpthw/ex9.py
@@ -0,0 +1,14 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun" #This is just a regular viarable with a string.
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"# This variable string has a line break function to separate the months in new lines
+
+print("Here are the days: ", days)# Standard print function with the added variable (line3)
+print("Here are the months: ", months)# Standard print function with the added variable (line4)
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as wel like.
+Even 4 lines if we want, or 5, or 6.
+""") # """ seems to allow the ability to break multiple lines in a single string
---
+++ sandlot.py
@@ -0,0 +1,14 @@
+test = 'practice'
+print (f"This is a {test}")
+excel = "I like to practice because it makes me be better at {}"
+print(excel.format('life'))
+print(excel.format('work'))
+print(excel.format('love'))
+print(excel.format('everything'))
+
+do_not = 'Don\'t'
+forget = "be brash, be rash understand that being rash is always better than being brash or having a rash"
+print(f"{do_not} {forget}")
+
+print(1 + 2, 2 > 3, 4 < 5, 2 * 3, 10 % 100)
+
